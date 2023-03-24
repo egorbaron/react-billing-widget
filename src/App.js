4aba3e5613edd5edb4aff91aafdf564b3b6df5f8
egorbaron <brnegor@gmail.com>
2023-03-24 16:44:39 +0200

    check state

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [toggle, setToggle] = useState(false);
+  const onClick = () => setToggle(!toggle);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {toggle && <img src={logo} className="App-logo" alt="logo" />}
+        <div onClick={onClick}>toggle</div>
       </header>
     </div>
   );
