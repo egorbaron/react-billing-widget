92eccf98830f6eeb7750033086053c96713411bb
egorbaron <brnegor@gmail.com>
2023-03-24 17:01:36 +0200

    check parent id

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ import './App.css';
 function App() {
   const [toggle, setToggle] = useState(false);
   const onClick = () => setToggle(!toggle);
+  const onCheck = () => {
+    console.log(document.getElementById("blueimp-gallery"))
+  }
   return (
     <div className="App">
       <header className="App-header">
         {toggle && <img src={logo} className="App-logo" alt="logo" />}
         <div onClick={onClick}>toggle</div>
+        <div onClick={onClick}>check</div>
       </header>
     </div>
   );
