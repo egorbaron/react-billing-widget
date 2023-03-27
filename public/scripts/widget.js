6205a2ca6563da3e3dd67d67c81a46b31b471296
egorbaron <brnegor@gmail.com>
2023-03-27 19:41:01 +0300

    fix iframe src

--- public/scripts/widget.js
+++ public/scripts/widget.js
@@ -1,6 +1,6 @@
 function initTestWidget () {
   const iframe = document.createElement("iframe");
-  iframe.src = "https://test-react-widget.vercel.app/scripts/widget.js";
+  iframe.src = "https://test-react-widget.vercel.app/";
   iframe.style.cssText = "height: 100% !important; width: 100% !important; position: fixed !important; border: 0px !important; inset: 0px !important; max-height: 100% important; z-index: 99999 !important";
   document.body.appendChild(iframe);
   const receiveMessage = (e) => {
