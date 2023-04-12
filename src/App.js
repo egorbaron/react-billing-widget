831ef1ba5875fbd19276eab10b57cd6d9a739c27
Home <home@MacBook-Pro-Egor.local>
2023-04-12 13:22:42 +0300

    changed name

--- src/App.js
+++ src/App.js
@@ -10,20 +10,20 @@ function App() {
 
   const onLoad = () => {
     window.top.postMessage({
-      test_widget: { type: messageEvents.loaded },
+      billing_widget: { type: messageEvents.loaded },
     }, "*");
     setLoading(false)
   }
   const onClose = () => {
     window.top.postMessage({
-      test_widget: { type: messageEvents.closeIframe },
+      billing_widget: { type: messageEvents.closeIframe },
     }, "*");
   };
 
   const receiveMessage = (e) => {
-    const type = e.data?.test_widget?.type;
+    const type = e.data?.billing_widget?.type;
     if (type === messageEvents.sendData) {
-      const url = e.data?.test_widget?.data?.payUrl;
+      const url = e.data?.billing_widget?.data?.payUrl;
       if (url) {
         setPayUrl(url);
       }
@@ -33,7 +33,7 @@ function App() {
   useLayoutEffect(() => {
     window.addEventListener("message", receiveMessage);
     window.parent.postMessage({
-      test_widget: { type: messageEvents.getData }
+      billing_widget: { type: messageEvents.getData }
     }, "*");
     setLoading(true);
     return () => window.removeEventListener("message", receiveMessage);
